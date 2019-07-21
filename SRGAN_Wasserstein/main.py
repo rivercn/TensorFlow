bf8bc8703d89036248cce8ca585b671a59c3cbbc
Aiin <34294025+MXbillow@users.noreply.github.com>
2019-07-21 16:30:44 +0800

    done

--- SRGAN_Wasserstein/main.py
+++ SRGAN_Wasserstein/main.py
@@ -1,7 +1,7 @@
-from Add.model import *
+from model import *
 from utils import *
 from config import config
-from Add.resnet50 import Resnet50
+from pretrain.nets.resnet_v1 import resnet_v1_50
 
 #优化器
 batch_size   = config.TRAIN.batch_size
@@ -25,7 +25,7 @@ def read_all_imgs(img_list, path='', n_threads=32):
     for idx in range(0, len(img_list), n_threads):
         b_imgs_list = img_list[idx : idx + n_threads]
         b_imgs = tl.prepro.threading_data(b_imgs_list, fn=get_imgs_fn, path=path)
-        # print(b_imgs.shape)
+
         imgs.extend(b_imgs)
         print('read %d from %s' % (len(imgs), path))
     return imgs
@@ -47,26 +47,18 @@ def train():
 
     ## If your machine have enough memory, please pre-load the whole train set.
     train_hr_imgs = read_all_imgs(train_hr_img_list, path=config.TRAIN.hr_img_path, n_threads=32)
-    # for im in train_hr_imgs:
-    #     print(im.shape)
-    # valid_lr_imgs = read_all_imgs(valid_lr_img_list, path=config.VALID.lr_img_path, n_threads=32)
-    # for im in valid_lr_imgs:
-    #     print(im.shape)
-    # valid_hr_imgs = read_all_imgs(valid_hr_img_list, path=config.VALID.hr_img_path, n_threads=32)
-    # for im in valid_hr_imgs:
-    #     print(im.shape)
-    # exit()
+
 
     ###========================== DEFINE MODEL ============================###
     ## train inference
     t_image = tf.placeholder('float32', [batch_size, 96, 96, 3], name='t_image_input_to_SRGAN_generator')
     t_target_image = tf.placeholder('float32', [batch_size, 384, 384, 3], name='t_target_image')
 
-    siamese_res = Resnet50()
-    siamese_net = siamese_res.deepnn
-    net_g = SRGAN_g(t_image, is_train=True, reuse=False)
-    net_d, logits_real ,sigmoid_out_real= SRGAN_d(t_target_image, is_train=True, reuse=False)
-    _,     logits_fake ,sigmoid_out_fake= SRGAN_d(net_g.outputs, is_train=True, reuse=True)
+    siamese_res = resnet_v1_50
+
+    net_g = SRGAN_g(t_image, siamese_res, reuse=False)
+    net_d, logits_real ,sigmoid_out_real= SRGAN_d(t_target_image, , reuse=False)
+    _,     logits_fake ,sigmoid_out_fake= SRGAN_d(net_g.outputs, siamese_res, reuse=True)
 
     net_g.print_params(False)
     net_d.print_params(False)
@@ -157,16 +149,6 @@ def train():
         epoch_time = time.time()
         total_mse_loss, n_iter = 0, 0
 
-        ## If your machine cannot load all images into memory, you should use
-        ## this one to load batch of images while training.
-        # random.shuffle(train_hr_img_list)
-        # for idx in range(0, len(train_hr_img_list), batch_size):
-        #     step_time = time.time()
-        #     b_imgs_list = train_hr_img_list[idx : idx + batch_size]
-        #     b_imgs = tl.prepro.threading_data(b_imgs_list, fn=get_imgs_fn, path=config.TRAIN.hr_img_path)
-        #     b_imgs_384 = tl.prepro.threading_data(b_imgs, fn=crop_sub_imgs_fn, is_random=True)
-        #     b_imgs_96 = tl.prepro.threading_data(b_imgs_384, fn=downsample_fn)
-
         ## If your machine have enough memory, please pre-load the whole train set.
         for idx in range(0, len(train_hr_imgs), batch_size):
             step_time = time.time()
@@ -213,16 +195,6 @@ def train():
         epoch_time = time.time()
         total_d_loss, total_g_loss, n_iter = 0, 0, 0
 
-        ## If your machine cannot load all images into memory, you should use
-        ## this one to load batch of images while training.
-        # random.shuffle(train_hr_img_list)
-        # for idx in range(0, len(train_hr_img_list), batch_size):
-        #     step_time = time.time()
-        #     b_imgs_list = train_hr_img_list[idx : idx + batch_size]
-        #     b_imgs = tl.prepro.threading_data(b_imgs_list, fn=get_imgs_fn, path=config.TRAIN.hr_img_path)
-        #     b_imgs_384 = tl.prepro.threading_data(b_imgs, fn=crop_sub_imgs_fn, is_random=True)
-        #     b_imgs_96 = tl.prepro.threading_data(b_imgs_384, fn=downsample_fn)
-
         ## If your machine have enough memory, please pre-load the whole train set.
         for idx in range(0, len(train_hr_imgs), batch_size):
             step_time = time.time()
@@ -265,35 +237,24 @@ def evaluate():
     checkpoint_dir = "checkpoint"
 
     ###====================== PRE-LOAD DATA ===========================###
-    # train_hr_img_list = sorted(tl.files.load_file_list(path=config.TRAIN.hr_img_path, regx='.*.jpg', printable=False))
-    # train_lr_img_list = sorted(tl.files.load_file_list(path=config.TRAIN.lr_img_path, regx='.*.jpg', printable=False))
     valid_hr_img_list = sorted(tl.files.load_file_list(path=config.VALID.hr_img_path, regx='.*.png', printable=False))
     valid_lr_img_list = sorted(tl.files.load_file_list(path=config.VALID.lr_img_path, regx='.*.png', printable=False))
 
     ## If your machine have enough memory, please pre-load the whole train set.
-    # train_hr_imgs = read_all_imgs(train_hr_img_list, path=config.TRAIN.hr_img_path, n_threads=32)
-    # for im in train_hr_imgs:
-    #     print(im.shape)
     valid_lr_imgs = read_all_imgs(valid_lr_img_list, path=config.VALID.lr_img_path, n_threads=32)
-    # for im in valid_lr_imgs:
-    #     print(im.shape)
+
     valid_hr_imgs = read_all_imgs(valid_hr_img_list, path=config.VALID.hr_img_path, n_threads=32)
-    # for im in valid_hr_imgs:
-    #     print(im.shape)
-    # exit()
+
 
     ###========================== DEFINE MODEL ============================###
     imid = 64 # 0: 企鹅  81: 蝴蝶 53: 鸟  64: 古堡
     valid_lr_img = valid_lr_imgs[imid]
     valid_hr_img = valid_hr_imgs[imid]
-    #img_name = '0010_80.jpg'
-    #valid_lr_img = get_imgs_fn(img_name, '/home/ubuntu/dataset/sr_test/testing/')  # if you want to test your own image
     valid_lr_img = (valid_lr_img / 127.5) - 1   # rescale to ［－1, 1]
-    # print(valid_lr_img.min(), valid_lr_img.max())
 
     size = valid_lr_img.shape
+
     t_image = tf.placeholder('float32', [None, size[0], size[1], size[2]], name='input_image')
-    # t_image = tf.placeholder('float32', [1, None, None, 3], name='input_image')
 
     net_g = SRGAN_g(t_image, is_train=False, reuse=False)
 
@@ -309,23 +270,15 @@ def evaluate():
 
     print("LR size: %s /  generated HR size: %s" % (size, out.shape)) # LR size: (339, 510, 3) /  gen HR size: (1, 1356, 2040, 3)
     print("[*] save images")
-    #tl.vis.save_image(out[0], save_dir+ '/gen_' + img_name[:-4] + '.png')
     tl.vis.save_image(out[0], save_dir + '/valid_gen.png')
-    #tl.vis.save_image(valid_lr_img, save_dir+'/valid_lr.png')
-    #tl.vis.save_image(valid_hr_img, save_dir+'/valid_hr.png')
-
     out_bicu = scipy.misc.imresize(valid_lr_img, [size[0]*4, size[1]*4], interp='bicubic', mode=None)
-    #tl.vis.save_image(out_bicu, save_dir + '/bicubic_' + img_name[:-4] + '.png')
     tl.vis.save_image(out_bicu, save_dir + '/valid_bicubic.png')
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--mode', type=str, default='srgan', help='srgan, evaluate')
-
     args = parser.parse_args()
-
     tl.global_flag['mode'] = args.mode
 
     if tl.global_flag['mode'] == 'srgan':
