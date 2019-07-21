1d93915fc72a7ea9629c491a470fe25bb3c9b3a4
Aiin <34294025+MXbillow@users.noreply.github.com>
2019-07-21 11:32:34 +0800

    done

--- SRGAN_Wasserstein/pretrain/datasets/download_and_convert_flowers.py
+++ SRGAN_Wasserstein/pretrain/datasets/download_and_convert_flowers.py
@@ -187,7 +187,7 @@ def run(dataset_dir):
     print('Dataset files already exist. Exiting without re-creating them.')
     return
 
-  dataset_utils.download_and_uncompress_tarball(_DATA_URL, dataset_dir)
+  #dataset_utils.download_and_uncompress_tarball(_DATA_URL, dataset_dir)
   photo_filenames, class_names = _get_filenames_and_classes(dataset_dir)
   class_names_to_ids = dict(zip(class_names, range(len(class_names))))
 
